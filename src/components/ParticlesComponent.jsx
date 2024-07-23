d789771249827967fd07a63db3ac6beb61acaad8
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 21:51:32 +0700

    create ParticleComponents

---
+++ src/components/ParticlesComponent.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesComponent = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = () => {};
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent",
+          opacity: 0.5,
+        },
+      },
+
+      fpsLimit: 120,
+      particles: {
+        color: {
+          value: "#B4E925",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.1,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 0.8,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.8,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={init}
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    </>
+  );
+};
+
+export default ParticlesComponent;
