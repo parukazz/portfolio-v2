8f568edcc172f70928bcbb282886e99a1993dd60
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 22:44:03 +0700

    add w-max class

--- src/components/ButtonScroll.jsx
+++ src/components/ButtonScroll.jsx
@@ -3,7 +3,7 @@ import ArrowDown from "./icons/ArrowDown";
 const ButtonScroll = () => {
   return (
     <>
-      <button className="border border-txt px-[10px] py-5 rounded-3xl">
+      <button className="border border-txt px-[10px] py-5 rounded-3xl w-max">
         <div className="animate-bounce">
           <ArrowDown color="#ffffff" />
         </div>
