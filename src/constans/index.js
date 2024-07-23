8375e25f05816f271b6801364397c5136ced21a6
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 21:53:56 +0700

    add some content "tagline, etc"

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,3 +1,6 @@
 import heroVideo from "../assets/videos/hero-video.mp4";
 
 export const heroBG = heroVideo;
+
+export const tagline = "Love build things for the web.";
+export const quoteHero = "Technology is anything that amplifies human potential. Any sufficiently advanced technology is equivalent to magic.";
