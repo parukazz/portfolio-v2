deae37d94308d73d6188d8b03576f4561216513d
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 22:45:08 +0700

    add navigation object

--- src/constans/index.js
+++ src/constans/index.js
@@ -4,3 +4,9 @@ export const heroBG = heroVideo;
 
 export const tagline = "Love build things for the web.";
 export const quoteHero = "Technology is anything that amplifies human potential. Any sufficiently advanced technology is equivalent to magic.";
+
+export const navigation = [
+  {id: 0, label: "about", href: "#about"},
+  {id: 1, label: "projects", href: "#projects"},
+  {id: 2, label: "contact", href: "#contact"},
+];
