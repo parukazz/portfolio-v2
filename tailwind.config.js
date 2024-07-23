a5de7b3dc6314277f742939f11062d744182d4e2
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 10:51:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "0.75rem",
+        sm: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
+        xl: "2.5rem",
+        "2xl": "3rem",
+      },
+      colors: {
+        primary: "#121212",
+        accent: "#B4E925",
+        txt: {
+          DEFAULT: "#FFFFFF",
+          muted: "#B0B0B0",
+        },
+      },
+      fontSize: {
+        brand: "8.875rem",
+        heading: "3.75rem",
+        paragraph: "1.125rem",
+      },
+    },
+  },
+  plugins: [],
+};
