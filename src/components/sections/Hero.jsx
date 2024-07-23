d0c2a642b37fe1bf61389235c5dfc2a89f84ae18
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 22:44:56 +0700

    add responsive style

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -11,18 +11,20 @@ const Hero = () => {
     <div className="text-txt px-[5%] lg:px-[70px]">
       <ParticlesComponent />
       <Header />
-      <div className="relative w-full min-h-screen flex items-end pb-24">
-        <div className="flex w-full items-center justify-between gap">
-          <div className="uppercase font-extrabold leading-none">
-            <h1 className="text-brand">paruk</h1>
-            <h1 className="text-brand">azziyi</h1>
+      <div className="relative w-full min-h-screen flex items-end pb-12 md:pb-24">
+        <div className="flex flex-col md:flex-row w-full items-center justify-between gap-4 md:gap-0">
+          <div className="uppercase font-extrabold leading-none text-6xl md:text-brand">
+            <h1>paruk</h1>
+            <h1>azziyi</h1>
           </div>
-          <div className="w-[400px]">
-            <p className="text-2xl font-medium mb-[14px]">{tagline}</p>
-            <p className="text-paragraph leading-7 text-txt-muted">
+          <div className="w-full md:w-[400px] text-center md:text-start">
+            <p className="hidden md:inline-block text-base md:text-2xl font-medium mb-[14px]">
+              {tagline}
+            </p>
+            <p className="mx-[15%] md:mx-0 text-sm md:text-paragraph leading-7 text-txt-muted">
               {quoteHero}
             </p>
-            <div className="flex mt-16 gap-[30px]">
+            <div className="flex flex-col-reverse md:flex-row items-center mt-16 gap-[30px]">
               <ButtonScroll />
               <div className="flex items-center gap-5">
                 <a href="#">
