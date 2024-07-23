2733b23609642f1007d67e87b22d17e0f16e3921
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 21:50:49 +0700

    create ButtonScroll components

---
+++ src/components/ButtonScroll.jsx
@@ -0,0 +1,15 @@
+import ArrowDown from "./icons/ArrowDown";
+
+const ButtonScroll = () => {
+  return (
+    <>
+      <button className="border border-txt px-[10px] py-5 rounded-3xl">
+        <div className="animate-bounce">
+          <ArrowDown color="#ffffff" />
+        </div>
+      </button>
+    </>
+  );
+};
+
+export default ButtonScroll;
