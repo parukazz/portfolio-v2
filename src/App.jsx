4b0ebeb5bd8d9b10c6c6737511f8f39dd3c49a8c
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-23 15:09:50 +0700

    add Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from './components/Header';
+import Hero from './components/sections/Hero';
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Hero />
     </div>
   )
 }
