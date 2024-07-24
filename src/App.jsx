7c22bb83bbe1dcf6c70a84fef61093a976c1d80d
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-24 14:16:16 +0700

    add new section (about)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from './components/sections/About';
 import Hero from './components/sections/Hero';
 
 const App = () => {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   )
 }
