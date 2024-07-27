930f7de6946588ac7dbf3e0646095925bda6cba3
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-27 20:57:11 +0700

    add new section (TechStack)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import About from './components/sections/About';
-import Hero from './components/sections/Hero';
+import About from "./components/sections/About";
+import Hero from "./components/sections/Hero";
+import TechStack from "./components/sections/TechStack";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <About />
+      <TechStack />
     </div>
-  )
-}
+  );
+};
 
 export default App;
