092e31f4e5999c451fcb2bf8c732636bc7c6aef4
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-27 20:59:11 +0700

    make title component flexible with some properties

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -1,15 +1,34 @@
 import rectangleIcon from "../assets/icons/rectangle.svg";
 
 // eslint-disable-next-line react/prop-types
-const TitleSection = ({ section, title }) => {
+const TitleSection = ({ section, title, titleCenter, sectionCenter }) => {
+  const classes = `flex items-center gap-[10px] absolute top-0 bottom-0 left-0 right-0 ${
+    titleCenter ? "justify-center" : "justify-start"
+  }`;
+
+  const sectionClasses = `relative flex ${
+    sectionCenter ? "justify-center" : "justify-start"
+  }`;
+
   return (
-    <div className="relative">
-      <span className="font-extrabold text-8xl bg-gradient-to-b from-zinc-600 to-zinc-950 bg-clip-text">
+    <div className={sectionClasses}>
+      <span className="font-extrabold text-6xl md:text-8xl bg-gradient-to-b from-zinc-600 to-zinc-950 bg-clip-text">
         {section}
       </span>
-      <div className="flex items-center gap-[10px] absolute top-0 bottom-0 left-0">
-        <img src={rectangleIcon} alt="icon" className="animate-spin-slow" />
-        <p className="text-2xl font-medium">{title}</p>
+      <div className={classes}>
+        <img
+          src={rectangleIcon}
+          alt="icon"
+          className="animate-spin-slow w-6 md:w-max"
+        />
+        <p className="text-base md:text-2xl font-medium">{title}</p>
+        <img
+          src={rectangleIcon}
+          alt="icon"
+          className={`animate-spin-slow w-6 md:w-max ${
+            titleCenter && sectionCenter ? "inline-block" : "hidden"
+          }`}
+        />
       </div>
     </div>
   );
