7973e26124f41ad246ecd57b39f6c7750fd0445b
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-27 20:59:31 +0700

    add photo 1 & 2

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,28 +1,41 @@
 import { abouttext, roletitle } from "../../constans";
 import Button from "../Button";
 import TitleSection from "../TitleSection";
+import Photo1 from "../../assets/photo-1.png";
+import Photo2 from "../../assets/photo-2.png";
 
 const About = () => {
   return (
     <section className="bg-primary relative px-[5%] md:px-[70px] py-40 w-full">
       <TitleSection section="ABOUT" title="WHO I AM" />
-      <div className="w-full flex justify-end pt-16">
-        <div className="flex w-5/6 h-96">
-          <div className="flex justify-between">
-            <div className="w-1/3 uppercase text-heading font-semibold flex flex-col leading-none gap-2">
+      <div className="w-full flex md:justify-end pt-32">
+        <div className="flex flex-col w-full md:w-5/6 gap-32">
+          <div className="flex flex-col md:flex-row justify-between gap-9 md:gap-0">
+            <div className="w-full md:w-1/3 uppercase text-5xl md:text-heading font-semibold flex flex-col leading-none gap-2">
               {roletitle.map((item, index) => (
                 <h2 key={index}>{item.label}</h2>
               ))}
             </div>
-            <div className="w-1/3">
+            <div className="w-full md:w-1/3">
               <div className="flex flex-col gap-5 mb-[50px]">
                 {abouttext.map((item, index) => (
-                  <p key={index} className="text-txt-muted text-paragraph">
+                  <p
+                    key={index}
+                    className="text-txt-muted text-base md:text-paragraph"
+                  >
                     {item.text}
                   </p>
                 ))}
               </div>
               <Button />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-9 md:gap-0">
+            <div className="">
+              <img src={Photo1} alt="" />
+            </div>
+            <div className="">
+              <img src={Photo2} alt="" />
             </div>
           </div>
         </div>
