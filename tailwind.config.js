27edf1b5b3c3f2c66099b50525dd836f0cdbedca
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-27 20:56:51 +0700

    add new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,8 +26,15 @@ export default {
         paragraph: "1.125rem",
       },
       animation: {
-        'spin-slow': "spin 3s linear infinite",
-      }
+        "spin-slow": "spin 3s linear infinite",
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
