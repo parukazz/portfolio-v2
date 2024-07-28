46b0af41334f698e79680d122749817d8bd0efb4
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-28 14:58:47 +0700

    modified button components (add new prop)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable react/prop-types */
 import ArrowRight from "../components/icons/ArrowRight";
 
-const Button = () => {
+const Button = ({ href, label }) => {
   return (
-    <button className="w-max -ml-2 flex items-center gap-1 text-paragraph font-semibold hover:translate-x-2 transition duration-200">
+    <a
+      href={href}
+      className="w-max -ml-2 flex items-center gap-1 text-paragraph font-semibold hover:translate-x-2 transition duration-200"
+      target="_blank"
+    >
       <ArrowRight className="fill-accent" size="24" />
-      LET&apos;S TALK
-    </button>
+      {label}
+    </a>
   );
 };
 
