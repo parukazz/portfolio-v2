bec707faf009ffc1dacc72cbd5387b4ae0509aea
Paruk Azziyi <parukazziyi@gmail.com>
2024-07-28 14:59:10 +0700

    change logo components more flexible

--- src/components/LogoSlider.jsx
+++ src/components/LogoSlider.jsx
@@ -1,12 +1,16 @@
-import { logoTech } from "../constans";
+import CSSIcon from "./icons/CSSIcon";
+import JSIcon from "./icons/JSIcon";
+import REACTIcon from "./icons/REACTIcon";
+import TailwindIcon from "./icons/TailwindIcon";
 
 const LogoSlider = () => {
   return (
-    <div className="w-full flex justify-center py-16">
-      <div className="flex w-max space-x-16 ">
-        {logoTech.map((item, index) => (
-          <img key={index} src={item.src} />
-        ))}
+    <div className="w-full flex justify-center py-16 overflow-hidden">
+      <div className="flex space-x-7 md:space-x-16 justify-center">
+        <CSSIcon className="fill-white group-hover:fill-accent" hover />
+        <JSIcon className="fill-white group-hover:fill-accent" hover />
+        <REACTIcon className="fill-white group-hover:fill-accent" hover />
+        <TailwindIcon className="fill-white group-hover:fill-accent" hover />
       </div>
     </div>
   );
