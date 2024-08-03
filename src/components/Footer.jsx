f807a43eeeab0bf6cbee69802d9a9d5dbd07de6d
Paruk Azziyi <parukazziyi@gmail.com>
2024-08-03 16:46:02 +0700

    create new component 'footer'

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-10 md:bottom-[72px] uppercase text-center">
+      <h1 className="text-heading md:text-brand font-extrabold">paruk azziyi</h1>
+      <p className="font-medium text-[12px] md:text-base">&copy; 2024. all rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
