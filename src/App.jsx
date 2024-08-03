d4ad39aaac4222944aef2483eb1069e53bfeb8d8
Paruk Azziyi <parukazziyi@gmail.com>
2024-08-03 16:44:50 +0700

    add new component section 'contact & credit'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import About from "./components/sections/About";
+import Contact from "./components/sections/Contact";
+import Credit from "./components/sections/Credit";
 import Hero from "./components/sections/Hero";
 import Projects from "./components/sections/Projects";
 import TechStack from "./components/sections/TechStack";
@@ -10,6 +12,8 @@ const App = () => {
       <About />
       <TechStack />
       <Projects />
+      <Contact />
+      <Credit />
     </div>
   );
 };
