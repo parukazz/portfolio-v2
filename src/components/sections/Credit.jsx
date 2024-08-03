ccd21c0b8d31332f1c175048657ed3c93f1ff67f
Paruk Azziyi <parukazziyi@gmail.com>
2024-08-03 16:46:46 +0700

    create new section 'credit'

---
+++ src/components/sections/Credit.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Footer";
+
+const Credit = () => {
+  return (
+    <section className="bg-photo-footer bg-center bg-cover bg-no-repeat w-full h-screen md:min-h-[1156px] relative flex justify-center items-center">
+      <div className="w-1/2 md:w-[420px]">
+        <p className="font-medium text-sm md:text-2xl">
+          Technology is my universe. My mission is to create, innovate, and
+          contribute, making this universe ever better and more thrilling.
+        </p>
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Credit;
