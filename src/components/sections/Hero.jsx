af111d1d9ab203666cca084c25ce861175ffb3fe
parukrev <xavierrevian@gmail.com>
2024-08-06 11:33:15 +0700

    add popup element motion

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,6 +6,16 @@ import SocialIcon from "../SocialIcon";
 import { motion } from "framer-motion";
 
 const Hero = () => {
+  const popUpElements = {
+    hidden: { opacity: 0, scale: 0.5 },
+    show: { opacity: 1, scale: 1 },
+    transition: {
+      duration: 0.8,
+      delay: 0.5,
+      ease: [0, 0.71, 1, 1.01],
+    },
+  };
+
   return (
     <div className="text-txt px-[5%] lg:px-[70px]">
       <ParticlesComponent />
@@ -17,7 +27,6 @@ const Hero = () => {
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.8,
-              delay: 0.5,
               ease: [0, 0.71, 1, 1.01],
             }}
             className="uppercase font-extrabold leading-none text-6xl md:text-brand"
@@ -26,12 +35,28 @@ const Hero = () => {
             <h1>azziyi</h1>
           </motion.div>
           <div className="w-full md:w-[400px] text-center md:text-start">
-            <p className="hidden md:inline-block text-base md:text-2xl font-medium mb-[14px]">
+            <motion.p 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 1, 1.01],
+            }}
+            className="hidden md:inline-block text-base md:text-2xl font-medium mb-[14px]">
               {tagline}
-            </p>
-            <p className="mx-[15%] md:mx-0 text-sm md:text-paragraph leading-7 text-txt-muted">
+            </motion.p>
+            <motion.p 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 1,
+              ease: [0, 0.71, 1, 1.01],
+            }}
+            className="mx-[15%] md:mx-0 text-sm md:text-paragraph leading-7 text-txt-muted">
               {quoteHero}
-            </p>
+            </motion.p>
             <div className="flex flex-col-reverse md:flex-row items-center mt-16 gap-[30px]">
               <ButtonScroll />
               <SocialIcon gap="gap-5" />
