514d12091c8e0d95e3041f072d3b7d05efa6ec2f
parukrev <xavierrevian@gmail.com>
2024-08-06 11:32:53 +0700

    add fadedown motion

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -2,24 +2,42 @@
 import GithubIcon from "./icons/GithubIcon";
 import InstagramIcon from "./icons/InstagramIcon";
 import LinkedinIcon from "./icons/LinkedinIcon";
+import { delay, motion } from "framer-motion";
 
 const SocialIcon = ({ iconSize = 24, white, gap, hidden }) => {
-
-  const classes = `icon-hero ${white ? "fill-txt" : "fill-txt-muted"}`
+  const classes = `icon-hero ${white ? "fill-txt" : "fill-txt-muted"}`;
   const dissapear = `${hidden ? "hidden" : ""}`;
 
+  const fadeDownVariants = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
+
   return (
-    <div className={`flex w-max items-center ${gap} ${dissapear}`}>
-      <a href="#">
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+      className={`flex w-max items-center ${gap} ${dissapear}`}
+    >
+      <motion.a variants={fadeDownVariants} href="#">
         <GithubIcon className={classes} size={iconSize} />
-      </a>
-      <a href="#">
+      </motion.a>
+      <motion.a variants={fadeDownVariants} href="#">
         <InstagramIcon className={classes} size={iconSize} />
-      </a>
-      <a href="#">
+      </motion.a>
+      <motion.a variants={fadeDownVariants} href="#">
         <LinkedinIcon className={classes} size={iconSize} />
-      </a>
-    </div>
+      </motion.a>
+    </motion.div>
   );
 };
 
