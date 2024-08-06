eb5a3978fed9d51cf106263716b42bff75635586
parukrev <xavierrevian@gmail.com>
2024-08-06 11:32:03 +0700

    add fadedown motion

--- src/components/ButtonScroll.jsx
+++ src/components/ButtonScroll.jsx
@@ -1,13 +1,25 @@
 import ArrowDown from "./icons/ArrowDown";
+import { motion } from "framer-motion";
 
 const ButtonScroll = () => {
+  const fadeDownVariants = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring", delay: 1.5 } },
+  };
+
   return (
     <>
-      <button className="border border-txt px-[10px] py-5 rounded-3xl w-max">
+      <motion.button
+        variants={fadeDownVariants}
+        initial="hidden"
+        animate="show"
+        viewport={{ once: true }}
+        className="border border-txt px-[10px] py-5 rounded-3xl w-max"
+      >
         <div className="animate-bounce">
           <ArrowDown color="#ffffff" />
         </div>
-      </button>
+      </motion.button>
     </>
   );
 };
